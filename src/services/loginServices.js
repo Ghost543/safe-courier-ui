18a6a914409f397f48aaa4e84c20a0c78c78d89a
Muwanguzi Peter <petermuwanguzi51@gmail.com>
2021-06-20 22:31:29 +0300

    fix-3

--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,6 +1,7 @@
 import http from '../services/httpServices'
 
-const urlEndpoint = "https://safe-courier.herokuapp.com"
+const urlEndpoint = "https://safe-courier.herokuapp.com/api/v2/auth/login" || "http://localhost:3100/api/v2/auth/login"
+
 export function login(user) {
     return http.post(urlEndpoint,{
         email: user.email,
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import http from '../services/httpServices'
 
-const urlEndpoint = "https://safe-courier.herokuapp.com"
+const urlEndpoint = "https://safe-courier.herokuapp.com/api/v2/auth/signup" || "http://localhost:3100/api/v2/auth/signup"
 
 export function signup(user) {
     return http.post(urlEndpoint,{
